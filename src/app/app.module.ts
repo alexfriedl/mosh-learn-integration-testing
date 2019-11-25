cdad5b130c63d59921f3f2c631eedd6c62191081
alexfriedl <anecronika@gmx.de>
2019-11-25 10:07:00 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router'; 
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { TodosComponent } from './2-todos/todos.component';
-import { UserDetailsComponent } from './3-user-details/user-details.component';
-import { VoterComponent } from './1-voter/voter.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { TodosComponent } from "./2-todos/todos.component";
+import { UserDetailsComponent } from "./3-user-details/user-details.component";
+import { VoterComponent } from "./1-voter/voter.component";
 
-import { routes } from './app.routes';
-import { UsersComponent } from './users/users.component';
-import { NavComponent } from './nav/nav.component';
-import { HighlightDirective } from './highlight.directive'; 
+import { routes } from "./app.routes";
+import { UsersComponent } from "./users/users.component";
+//import { NavComponent } from './nav/nav.component';
+import { HighlightDirective } from "./highlight.directive";
 
 @NgModule({
   declarations: [
@@ -23,7 +23,7 @@ import { HighlightDirective } from './highlight.directive';
     UserDetailsComponent,
     VoterComponent,
     UsersComponent,
-    NavComponent,
+    //NavComponent,
     HighlightDirective
   ],
   imports: [
@@ -35,4 +35,4 @@ import { HighlightDirective } from './highlight.directive';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
